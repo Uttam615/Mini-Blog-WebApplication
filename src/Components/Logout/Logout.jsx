7a2062de4d75b80b21bb8c94505efd931793337f
Uttam615 <Uttamkumarkhuraijam733@gmail.com>
2025-05-20 19:17:18 +0530

    Update Logout component to clear token and redirect

--- src/Components/Logout/Logout.jsx
+++ src/Components/Logout/Logout.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Logout.css'
 
 const Logout = () => {
+  const naviagte = useNavigate()
+  useEffect(()=>{
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
+    naviagte('/login')
+  },[naviagte]);
   return (
     <div>
-        <h2>Logout Page</h2>
+        <h2>Logging Out</h2>
+
     </div>
   )
 }
